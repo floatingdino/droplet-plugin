d7b31d2abd64ae4c48fbd5831a382ed8ea7cbf01
Sam Haakman <sam@lambagency.com.au>
2019-08-30 08:37:16 +1000

    updated liquid syntax

--- droplet-plugin.js
+++ droplet-plugin.js
@@ -3,36 +3,34 @@ const pluginName = "DropletPlugin";
 class DropletPlugin {
   apply(compiler) {
     compiler.hooks.emit.tap(pluginName, compilation => {
-			const jsRX = /\.js$/
-			const assetNames = Object.keys(compilation.assets);
-			assetNames.forEach(assetName => {
-				if (!jsRX.test(assetName)) {
-					return;
-				}
+      const jsRX = /\.js$/;
+      const assetNames = Object.keys(compilation.assets);
+      assetNames.forEach(assetName => {
+        if (!jsRX.test(assetName)) {
+          return;
+        }
 
-				const parent_source = compilation.assets[assetName].source();
-				const liquid_bootstrap = `{% comment %}
+        const parent_source = compilation.assets[assetName].source();
+        const liquid_bootstrap = `{% comment %}
 
 Use the Shopify CDN as Webpack's publicPath so that dynamic code splitting works
 
 {% endcomment %}
-{% capture cdn_path_basis %}
-{{ '?' | asset_url }}
-{% endcapture %}
+{% capture cdn_path_basis %}{{ '?' | asset_url }}{% endcapture %}
 {% assign cdn_base = cdn_path_basis | split: '?' %}
 {% assign cdn_base = cdn_base[0] %}
-__webpack_public_path__ = {{ cdn_base }};`;
+__webpack_public_path__ = {{ cdn_base | json }};`;
 
-				compilation.assets[`${assetName}.liquid`] = {
-					source() {
-						return `${liquid_bootstrap}
-${parent_source}`
-					},
-					size() {
-						return parent_source.length + liquid_bootstrap.length;
-					}
-				}
-			})
+        compilation.assets[`${assetName}.liquid`] = {
+          source() {
+            return `${liquid_bootstrap}
+${parent_source}`;
+          },
+          size() {
+            return parent_source.length + liquid_bootstrap.length;
+          }
+        };
+      });
     });
   }
 }
